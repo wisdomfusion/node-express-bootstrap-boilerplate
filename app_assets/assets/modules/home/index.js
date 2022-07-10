ea5c6aace170b48ef373e6e74818ec293e90f148
WisdomFusion <WisdomFusion@gmail.com>
2022-07-11 05:42:51 +0800

    fix;

--- app_assets/assets/modules/home/index.js
+++ app_assets/assets/modules/home/index.js
@@ -1,1 +1,21 @@
 import './styles.scss';
+
+const homeModule = (function ($, window) {
+    'use strict';
+
+    const MODULE_NAME = 'MODULE-home';
+
+    // TODO
+
+    function init() {
+        console.log(MODULE_NAME);
+    }
+
+    return {
+        init,
+    }
+})(jQuery, window);
+
+jQuery(function () {
+    jQuery('body#MODULE-home').length && homeModule.init();
+});
